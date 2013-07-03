a63b4521dd2a216c0147c9a9555abc86da244a15
Paritosh Piplewar <paritosh@passion8.co.in>
2013-07-03 08:31:02 +0530

    rails default files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crm::Application.config.secret_token = 'fb635681ad588bf04dc55bb9cb3b1a10b36606c75de0107dae32bb57141d85d47f269ca4c56e32ccd482354c1a6c30aff002a575bec3f7b6614f24f3fded6ca0'
