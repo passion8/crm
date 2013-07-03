3bed010e50970535f46cdc2f6eca35fd2036e233
Paritosh Piplewar <paritosh@passion8.co.in>
2013-07-04 02:34:46 +0530

    configure for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Crm::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = { :host => 'fast-reef-6206.herokuapp.com' }
 end
